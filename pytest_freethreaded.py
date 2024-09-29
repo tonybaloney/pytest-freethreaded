428b93bde5a9489b0d63ccecc712613f376a47cb
NCLI <45665319+NCLI@users.noreply.github.com>
2024-09-29 02:44:54 +0000

    Update pytest_freethreaded.py
    One underscore too many!

--- pytest_freethreaded.py
+++ pytest_freethreaded.py
@@ -5,7 +5,7 @@ import sys
 @pytest.hookimpl
 def pytest_sessionstart(session):
     # See https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest.hookspec.pytest_sessionstart
-    assert sys.__is_gil_enabled() == False
+    assert sys._is_gil_enabled() == False
 
 @pytest.hookimpl
 def pytest_sessionfinish(session):
