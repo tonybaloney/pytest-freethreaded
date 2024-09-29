a82c53fb152ea57d09f9dccf657c677e64fa37d2
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-09-29 11:58:20 +0900

    Update plugin

--- src/pytest_freethreaded/plugin.py
+++ src/pytest_freethreaded/plugin.py
@@ -7,8 +7,8 @@ def pytest_sessionstart(session):
     # TODO: Assert that the GIL is disabled
     ...
 
+
 @pytest.hookimpl
 def pytest_sessionfinish(session):
     # See https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest.hookspec.pytest_sessionfinish
     ...
-
