c6ec7cae4e71d70dd4d13d6630b44cc39320206b
Mike Hommey <mh@glandium.org>
2024-09-29 15:40:50 +0900

    add a test with one thread, meant to pass

--- test_pytest_racy_fixture.py
+++ test_pytest_racy_fixture.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_freethreaded.plugin import ConcurrencyError
 
 
 class Counter:
@@ -19,27 +20,28 @@ def counter():
     return Counter()
 
 
-def test_counter(counter):
+def common_test_counter(counter):
     value = counter.value()
     counter.increment()
     counter.decrement()
     value_after = counter.value()
     assert value == value_after, f"{value} != {value_after}"
+
+
+def test_counter(counter):
+    common_test_counter(counter)
 
 
 @pytest.mark.freethreaded(threads=20, iterations=180)
 def test_counter_with_markers(counter):
-    value = counter.value()
-    counter.increment()
-    counter.decrement()
-    value_after = counter.value()
-    assert value == value_after, f"{value} != {value_after}"
+    common_test_counter(counter)
 
 
-@pytest.mark.freethreaded(iterations=180)
+@pytest.mark.freethreaded(iterations=100)
 def test_counter_with_markers_no_threads(counter):
-    value = counter.value()
-    counter.increment()
-    counter.decrement()
-    value_after = counter.value()
-    assert value == value_after, f"{value} != {value_after}"
+    common_test_counter(counter)
+
+
+@pytest.mark.freethreaded(threads=1, iterations=100)
+def test_counter_with_markers_single_thread(counter):
+    common_test_counter(counter)
