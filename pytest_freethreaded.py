43c297d2125818d15e0b243ab770212b176aa673
NCLI <45665319+NCLI@users.noreply.github.com>
2024-09-29 02:49:29 +0000

    Nicer style

--- pytest_freethreaded.py
+++ pytest_freethreaded.py
@@ -5,7 +5,7 @@ import sys
 @pytest.hookimpl
 def pytest_sessionstart(session):
     # See https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest.hookspec.pytest_sessionstart
-    assert sys._is_gil_enabled() == False
+    assert not sys._is_gil_enabled()
 
 @pytest.hookimpl
 def pytest_sessionfinish(session):
